55963bab5b4dc79ece2e2f00b98f3adea8b5f0f4
Meesala-BFRS01946 <110400906+meesala-BFRS01946@users.noreply.github.com>
2023-03-30 12:35:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify , redirect , url_for , render_template
+from flask import Flask, request, jsonify 
 import requests
+import re
 from dotenv import load_dotenv
 import openai
 import json
@@ -8,15 +9,15 @@ import logging
 from datetime import datetime
 from intents import intents
 from responses import responses
+import translators as ts
 
 load_dotenv()
-openai.api_key = "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"
+openai.api_key = "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"
 
 with open('phrases.json', 'r') as f:
     phrases = json.load(f)
 
 app = Flask(__name__)
-
 # set up logging
 logs_folder = "logs"
 if not os.path.exists(logs_folder):
@@ -27,10 +28,11 @@ logging.basicConfig(filename=log_filename, level=logging.DEBUG, format='%(asctim
 
 @app.route('/', methods=['POST'])
 def pred():
-    #try:
+    
     global text 
     data =request.get_json()
     text=data["text"]
+    text=translate_to_english(text)
     mapp = json.dumps(phrases)
     prompt = " by taking the reference from the following map {} , please map the following text {} to the one of the intent in the following intents {} and return NULL if no intents are matched".format(mapp, text, intents)
     response = openai.Completion.create(
@@ -43,30 +45,24 @@ def pred():
         presence_penalty=0.17)
     intent = ((response.choices[0].text).strip()).lower()
     logging.info("Received text: {}, Predicted intent: {}".format(text, intent))
-    if intent == "tracking details":
-        return jsonify({"response": tracking_order(),"intent":intent})
+    if is_number(text):
+        return jsonify({"response":tracking_order(text),"intent":"awb_number"})
+    elif intent == "tracking details":
+        return jsonify({"response": "please enter your AWB number","intent":intent})
     elif intent == "greeting":
-        return jsonify({"response": greeting(),"intent":intent})
-        #return redirect("/greeting",var=text)
+        return jsonify({"response": translate_to_english(greeting()),"intent":intent})
     elif intent == "greeting->greeting":
-        return jsonify({"response": greetingfallback(),"intent":intent})
+        return jsonify({"response": translate_to_english(greetingfallback()),"intent":intent})
     elif intent == "pickup not attempted":
-        return jsonify({"response": pickup_not_attempted(),"intent":intent})
+        return jsonify({"response": "please enter your AWB number","intent":intent})
     
-    return jsonify({"response": intent})
+    return jsonify({"response": "sorry i didn't understand can you say that again"})
 
-    # except Exception as e:
-    #     logging.exception("Error occurred: {}".format(str(e)))
-    #     return jsonify({"intent": "error"})
+    
 
-
-#@app.route('/tracking')
-def tracking_order():
-    awb="SRTP8501354758"
-    #awb=input("please enter the awb number")
+def tracking_order(awb):
+    #awb="SRTP8501354758"
     tracking_url= "https://apiv2.shiprocket.in/v1/tracking/{}".format(awb)
-    token=os.getenv("bearer_token")
-    headers = {'Authorization': f'Bearer {token}'}
     response = requests.get(tracking_url)
     try:
         if response.status_code == 200:
@@ -77,29 +73,11 @@ def tracking_order():
     except:
         return("Hi, we haven't found any AWB under this category.")
     
-def pickup_not_attempted():
-    awb_no="SRTP8501354758"
-    tracking_url= "https://apiv2.shiprocket.in/v1/tracking/{}".format(awb_no)
-    #token=os.getenv("bearer_token")
-    #headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(tracking_url)
-    # response_dict = response.json()
-    # awbs = response_dict["data"]["awbs"]
-    # awb_numbers = [awb["awb"] for awb in awbs]
-    # if awb_no in awb_numbers:
-    try:
-        if response.status_code == 200:
-            data = response.json()
-            r = data["tracking_data"]["shipment_track"][0]["current_status"]
-            rr= responses["track_order"].format(r)
-            return rr
-    except:
-        return("Hi, we haven't found any AWB under this category.")
 
     
 #@app.route('/greeting')
 def greeting():
-    prompt = "The user had got his query resolved in a e-commerce chatbot , please give reply to the following text as a e-commerce chatbot in english {} ".format(text)
+    prompt = "The user had got his query resolved in a e-commerce chatbot , please give reply to the following text as a e-commerce chatbot in english only {} ".format(text)
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
@@ -108,7 +86,7 @@ def greeting():
         top_p=0.54,
         frequency_penalty=0.44,
         presence_penalty=0.17)
-    return (response.choices[0].text).strip()
+    return str((response.choices[0].text).strip())
 def greetingfallback():
     prompt = "The user had got his query resolved in a e-commerce chatbot , please give reply to the following text as a e-commerce chatbot in english {} ".format(text)
     response = openai.Completion.create(
@@ -119,7 +97,19 @@ def greetingfallback():
         top_p=0.54,
         frequency_penalty=0.44,
         presence_penalty=0.17)
-    return (response.choices[0].text).strip()
+    return str((response.choices[0].text).strip())
+
+
+def translate_to_english(textt):
+  try:
+    result = ts.translate_text(textt, to_language='en')
+    return result
+  except:
+    return textt
+def is_number(input):
+    pattern = r'\d{9,}'
+    return bool(re.search(pattern, input))
+    
 
 
 
@@ -127,3 +117,4 @@ def greetingfallback():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
